5fc91d4b3aeaa4a45240e25f94142b2296cf08df
임채민 <coals1153@gmail.com>
2017-06-30 06:21:19 +0900

    complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,8 @@ class HomeController < ApplicationController
   end
 
   def game
-    
+    @left = rand(80)
+    @top = rand(80)
+    @result = 'left:' + @left.to_s + '%;' + 'top:' + @top.to_s + '%;'
   end
 end
